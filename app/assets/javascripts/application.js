9135f320f87e634fc42c343895edd1266c58e0e7
Yuichi Takeuchi <uzuki05@takeyu-web.com>
2015-09-15 00:50:01 +0900

    set port

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,6 +5,11 @@
 
 window.App = {};
 
+var protocol = document.getElementsByName('protocol')[0].content;
 var host = document.getElementsByName('host')[0].content;
 var port = document.getElementsByName('port')[0].content;
-window.App.cable = Cable.createConsumer("ws://"+host+":"+port);
+if (port && port != 80) {
+    window.App.cable = Cable.createConsumer("ws://"+host+":"+port);
+} else {
+    window.App.cable = Cable.createConsumer("ws://"+host);
+}

101afd6832600b5badcc086f23b7033caccd5785
Yuichi Takeuchi <uzuki05@takeyu-web.com>
2015-10-23 23:29:45 +0900

    AltContainer で require('react/addons') されているのでその帳尻あわせ･･･ react-rails の addons = true ではうまくいかなかったので vendor/bower_components 内のものを参照 多分僕の無知によるもの

--- app/assets/javascripts/app.jsx
+++ app/assets/javascripts/app.jsx
@@ -1,3 +1,6 @@
+// AltContainer で require('react/addons') されているのでその帳尻あわせ･･･
+// react-rails の addons = true ではうまくいかなかったので vendor/bower_components 内のものを参照
+// 多分僕の無知によるもの
 var React = require('react/react-with-addons');
 var Messages = require('./components/Messages.jsx');
 
--- app/assets/javascripts/react/addons.js
+++ app/assets/javascripts/react/addons.js
@@ -1,1 +1,4 @@
+// AltContainer で require('react/addons') されているのでその帳尻あわせ･･･
+// react-rails の addons = true ではうまくいかなかったので vendor/bower_components 内のものを参照
+// 多分僕の無知によるもの
 module.exports = require('react/react-with-addons');
